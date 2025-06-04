07b59ff76c8e59c45862e70763fa8fcf0877a78b
Olorunfemi Samuel <69216952+olorunfemisamuel@users.noreply.github.com>
2025-06-04 12:44:22 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,12 +9,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel = "stylesheet" href="/css/app.css">
+        
 
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite('ressources/css/app.css')
+        @vite('resources/css/app.css')
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
 
         @inertiaHead
